075086114aff9a6d6d1194bd30ddd89648635e22
SDSGK <1271745539@qq.com>
2022-01-20 15:10:30 +0800

    feat：细节优化
    1.优化子弹大小（初版）；
    2.优化爆炸动画大小。

--- js/canvas.js
+++ js/canvas.js
@@ -1,17 +1,17 @@
-
 // 初始化画布
 function initCanvas() {
-  clearCanvas()
+  clearCanvas();
   playerImage.onload = () => {
+    // 绘制玩家贴图
     ctx.drawImage(
-      playerImage, 
-      playerInfo.offsetLeft, 
-      playerInfo.offsetTop, 
-      playerInfo.width, 
+      playerImage,
+      playerInfo.offsetLeft,
+      playerInfo.offsetTop,
+      playerInfo.width,
       playerInfo.height
-    )
+    );
     ctx.stroke();
-  }
+  };
 }
 
 function clearCanvas() {
@@ -20,45 +20,52 @@ function clearCanvas() {
 
 // 绘制玩家位置
 function drawPlayer() {
+  // 保存上次绘图
+  ctx.save();
+  if (isInvincibleTimer) {
+    ctx.globalAlpha = 0.5;
+  } else {
+    ctx.globalAlpha = 1;
+  }
   ctx.drawImage(
-    playerImage, 
-    playerInfo.offsetLeft, 
-    playerInfo.offsetTop, 
-    playerInfo.width, 
+    playerImage,
+    playerInfo.offsetLeft,
+    playerInfo.offsetTop,
+    playerInfo.width,
     playerInfo.height
-  )
-  ctx.stroke();
+  );
+  ctx.restore();
 }
 // 绘制子弹
 function drewBullet() {
-  const bulletMap = bulletStore.getStore()
+  const bulletMap = bulletStore.getStore();
   for (const key in bulletMap) {
     const element = bulletMap[key].target || bulletMap[key];
     // 子弹的位置
-    const positionX = element.positionX - containerInfo.offsetLeft
-    const positionY = element.positionY - containerInfo.offsetTop
+    const positionX = element.positionX - containerInfo.offsetLeft;
+    const positionY = element.positionY - containerInfo.offsetTop;
     // 子弹的宽高度
-    const bulletWidth = element.width
-    const bulletHeight = element.height
+    const bulletWidth = element.width;
+    const bulletHeight = element.height;
     // 偏移量
-    const delayX = positionX + bulletWidth / 2
-    const delayY = positionY + bulletHeight / 2
+    const delayX = positionX + bulletWidth / 2;
+    const delayY = positionY + bulletHeight / 2;
     // 保存上次绘图
     ctx.save();
     // 进行中心点平移
-    ctx.translate(delayX, delayY)
+    ctx.translate(delayX, delayY);
     // 旋转画布
-    ctx.rotate(element.angle * Math.PI / 180);
+    ctx.rotate((element.angle * Math.PI) / 180);
     // 进行中心点归位
-    ctx.translate(-delayX, -delayY)
+    ctx.translate(-delayX, -delayY);
     // 绘制子弹图片
     ctx.drawImage(
-      bulletImage, 
+      bulletImage,
       positionX,
       positionY,
-      bulletWidth * 1.5, 
-      bulletHeight * 1.5
-    )
+      bulletWidth,
+      bulletHeight
+    );
     // 恢复上次保存的图
     ctx.restore();
   }
@@ -67,19 +74,19 @@ function drewBullet() {
 function drewEnemy() {
   for (const type in typeStore) {
     const store = typeStore[type];
-    const storeMap = store.getStore()
+    const storeMap = store.getStore();
     for (const key in storeMap) {
-      const element = storeMap[key]
+      const element = storeMap[key];
       // 敌机贴图
       ctx.drawImage(
-        element.canvasImage, 
-        element.positionX, 
-        element.positionY, 
-        element.width, 
+        element.canvasImage,
+        element.positionX,
+        element.positionY,
+        element.width,
         element.height
-      )
-      ctx.strokeStyle = '#ffffff'
-      ctx.lineWidth = 1; 
+      );
+      ctx.strokeStyle = "#ffffff";
+      ctx.lineWidth = 1;
       if (!element.isHideHealth) {
         // 血条
         ctx.strokeRect(
@@ -87,20 +94,22 @@ function drewEnemy() {
           element.positionY - 20,
           element.width,
           12
-        )
-        ctx.fillStyle = '#ff0000'
+        );
+        ctx.fillStyle = "#ff0000";
         ctx.fillRect(
           element.positionX + 1,
           element.positionY - 20 + 1,
-          ((element.target.health / element.target.healthOriginal * element.width) - 2) ,
-          12 -2
-        )
+          (element.target.health / element.target.healthOriginal) *
+            element.width -
+            2,
+          12 - 2
+        );
         // 血量文字
-        ctx.fillStyle = '#ffffff'
-        ctx.textAlign = 'center'
+        ctx.fillStyle = "#ffffff";
+        ctx.textAlign = "center";
         ctx.fillText(
-          element.target.health, 
-          element.positionX + element.width / 2, 
+          element.target.health,
+          element.positionX + element.width / 2,
           element.positionY - 10
         );
         ctx.stroke();
@@ -117,8 +126,8 @@ function setBoom(element) {
       boomImageArray[index++],
       element.positionX,
       element.positionY,
-      element.width,
-      element.height
+      80,
+      60
     );
     if (index >= 26) {
       clearInterval(intervalTime);
